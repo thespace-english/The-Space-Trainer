e3cd3888daccf5d79b54589a32550b12fdc99cfa
thespace-english <vyrezkova.dnschool@gmail.com>
2026-07-26 20:09:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/* FAVICON */
+if (!document.querySelector('link[rel="icon"]')) {
+    const favicon = document.createElement("link");
+    favicon.rel = "icon";
+    favicon.type = "image/png";
+    favicon.href = "https://thespace-english.github.io/EGE/favicon.png";
+    document.head.appendChild(favicon);
+}
+
 function openGeneral() {
 
     alert("SPACE GENERAL\n\nKids\nTeens");
